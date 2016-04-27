b5eca28f4a91ab015f1b4d5c22bb7c06ac4dc0c3
Grzegorz Dobiecki <grzegorz.dobiecki@gft.com>
2016-04-27 15:30:31 +0200

    refactor onClickListener

--- app/src/main/java/com/example/grdc/simplecalc/MainActivity.java
+++ app/src/main/java/com/example/grdc/simplecalc/MainActivity.java
@@ -1,18 +1,16 @@
 package com.example.grdc.simplecalc;
 
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String MESSAGE = "Give the numbers.";
     private Button btnAdd;
     private Button btnSubtraction;
@@ -29,58 +27,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         init();
-        final String num1 = etFirst.getText().toString();
-        final String num2 = etSecond.getText().toString();
-
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (TextUtils.isEmpty(num1) || TextUtils.isEmpty(num2)) {
-                    Toast.makeText(MainActivity.this, MESSAGE, Toast.LENGTH_SHORT).show();
-                } else {
-                    int addition = Integer.parseInt(num1) + Integer.parseInt(num2);
-                    result.setText(String.valueOf(addition));
-                }
-            }
-        });
-
-        btnSubtraction.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (TextUtils.isEmpty(num1) || TextUtils.isEmpty(num2)) {
-                    result.setText(MESSAGE);
-                } else {
-                    int subtraction = Integer.parseInt(num1) - Integer.parseInt(num2);
-                    result.setText(String.valueOf(subtraction));
-                }
-            }
-        });
-        btnMultiply.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (TextUtils.isEmpty(num1) || TextUtils.isEmpty(num2)) {
-                    result.setText(MESSAGE);
-                } else {
-                    int multiplication = Integer.parseInt(num1) * Integer.parseInt(num2);
-                    result.setText(String.valueOf(multiplication));
-                }
-            }
-        });
-        btnDivide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (TextUtils.isEmpty(num1) || TextUtils.isEmpty(num2)) {
-                    result.setText(MESSAGE);
-                } else {
-                    try {
-                        int division = Integer.parseInt(num1) / Integer.parseInt(num2);
-                        result.setText(String.valueOf(division));
-                    } catch (IllegalArgumentException e) {
-                        result.setText("Division by zero is not allowed.");
-                    }
-                }
-            }
-        });
+        btnAdd.setOnClickListener(this);
+        btnSubtraction.setOnClickListener(this);
+        btnMultiply.setOnClickListener(this);
+        btnDivide.setOnClickListener(this);
     }
 
     private void init() {
@@ -91,6 +41,49 @@ public class MainActivity extends AppCompatActivity {
         etFirst = (EditText) findViewById(R.id.firstNum);
         etSecond = (EditText) findViewById(R.id.secondNum);
         result = (TextView) findViewById(R.id.result);
+    }
 
+    @Override
+    public void onClick(View view) {
+        String num1 = etFirst.getText().toString();
+        String num2 = etSecond.getText().toString();
+        switch (view.getId()) {
+            case R.id.addBtn:
+                if (TextUtils.isEmpty(num1) || TextUtils.isEmpty(num2)) {
+                    Toast.makeText(MainActivity.this, MESSAGE, Toast.LENGTH_SHORT).show();
+                } else {
+                    int addition = Integer.parseInt(num1) + Integer.parseInt(num2);
+                    result.setText(String.valueOf(addition));
+                }
+                break;
+            case R.id.subtractionBtn:
+                if (TextUtils.isEmpty(num1) || TextUtils.isEmpty(num2)) {
+                    result.setText(MESSAGE);
+                } else {
+                    int subtraction = Integer.parseInt(num1) - Integer.parseInt(num2);
+                    result.setText(String.valueOf(subtraction));
+                }
+                break;
+            case R.id.multiplyBtn:
+                if (TextUtils.isEmpty(num1) || TextUtils.isEmpty(num2)) {
+                    result.setText(MESSAGE);
+                } else {
+                    int multiplication = Integer.parseInt(num1) * Integer.parseInt(num2);
+                    result.setText(String.valueOf(multiplication));
+                }
+                break;
+            case R.id.divideBtn:
+                if (TextUtils.isEmpty(num1) || TextUtils.isEmpty(num2)) {
+                    result.setText(MESSAGE);
+                } else {
+                    try {
+                        int division = Integer.parseInt(num1) / Integer.parseInt(num2);
+                        result.setText(String.valueOf(division));
+                    } catch (IllegalArgumentException e) {
+                        result.setText("Division by zero is not allowed.");
+                    }
+                }
+                break;
+        }
     }
 }
