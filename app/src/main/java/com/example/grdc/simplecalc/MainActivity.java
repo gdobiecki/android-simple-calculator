1832075d3acf386cfdde80dc6bca65a59d753f8e
Grzegorz Dobiecki <grzegorz.dobiecki@gft.com>
2016-04-27 15:17:06 +0200

    change logic

--- app/src/main/java/com/example/grdc/simplecalc/MainActivity.java
+++ app/src/main/java/com/example/grdc/simplecalc/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.grdc.simplecalc;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String MESSAGE = "Give the numbers.";
+    private Button btnAdd;
+    private Button btnSubtraction;
+    private Button btnMultiply;
+    private Button btnDivide;
+    private EditText etFirst;
+    private EditText etSecond;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,48 +28,69 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        init();
+        final String num1 = etFirst.getText().toString();
+        final String num2 = etSecond.getText().toString();
 
-        final Button button = (Button) findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
+        btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String pressed = button.getText().toString();
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(num1) || TextUtils.isEmpty(num2)) {
+                    Toast.makeText(MainActivity.this, MESSAGE, Toast.LENGTH_SHORT).show();
+                } else {
+                    int addition = Integer.parseInt(num1) + Integer.parseInt(num2);
+                    result.setText(String.valueOf(addition));
+                }
             }
         });
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        btnSubtraction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(num1) || TextUtils.isEmpty(num2)) {
+                    result.setText(MESSAGE);
+                } else {
+                    int subtraction = Integer.parseInt(num1) - Integer.parseInt(num2);
+                    result.setText(String.valueOf(subtraction));
+                }
+            }
+        });
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(num1) || TextUtils.isEmpty(num2)) {
+                    result.setText(MESSAGE);
+                } else {
+                    int multiplication = Integer.parseInt(num1) * Integer.parseInt(num2);
+                    result.setText(String.valueOf(multiplication));
+                }
+            }
+        });
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(num1) || TextUtils.isEmpty(num2)) {
+                    result.setText(MESSAGE);
+                } else {
+                    try {
+                        int division = Integer.parseInt(num1) / Integer.parseInt(num2);
+                        result.setText(String.valueOf(division));
+                    } catch (IllegalArgumentException e) {
+                        result.setText("Division by zero is not allowed.");
+                    }
+                }
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void init() {
+        btnAdd = (Button) findViewById(R.id.addBtn);
+        btnSubtraction = (Button) findViewById(R.id.subtractionBtn);
+        btnMultiply = (Button) findViewById(R.id.multiplyBtn);
+        btnDivide = (Button) findViewById(R.id.divideBtn);
+        etFirst = (EditText) findViewById(R.id.firstNum);
+        etSecond = (EditText) findViewById(R.id.secondNum);
+        result = (TextView) findViewById(R.id.result);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void countButtonClicked(View view) {
-        Snackbar.make(view, "dang", Snackbar.LENGTH_LONG).setAction("Action", null).show();
     }
 }
